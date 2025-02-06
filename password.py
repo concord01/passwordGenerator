8fac86de2d27bd211cbce8e617334717f5ae08b5
Joaquin Hoyem <joaquin.hoyem@gmail.com>
2025-02-06 18:57:32 +0000

    initial commit

---
+++ password.py
@@ -0,0 +1,28 @@
+import random
+import string
+import requests
+
+# function that generates a single random character
+def random_character():
+    choices = string.ascii_letters + string.digits + string.punctuation
+    return random.choice(choices)
+
+passwordLength = int(input("Input desired password length: \n"))
+
+# password generator function
+def password_generator():
+    password = ""
+    for i in range(passwordLength):
+        password = password + random_character()
+    print(password)
+password_generator()
+
+
+# function that gets a random word from a dictionary api
+def fetch_word():
+    url = "https://random-word-api.herokuapp.com/word?length=6"
+    response = requests.get(url)
+    word = response.json()[0]
+    return word
+
+print(fetch_word)
