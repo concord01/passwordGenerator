6e1875b03146c0a6f6e5be4c244784133ed4fd06
Joaquin Hoyem <joaquin.hoyem@gmail.com>
2025-02-12 18:25:47 +0000

    strengthened the weak generator :

--- password.py
+++ password.py
@@ -25,9 +25,25 @@ def fetch_word():
     word = response.json()[0]
     return word
 
+def replaceLetters(word):
+    word = word[0].upper() + word[1:]
+    if "a" in word:
+        word = word.replace("a", "@")
+    if "e" or "E" in word:
+        word = word.replace("e", "3")
+        word = word.replace("E", "3")
+    if "l" in word:
+        word = word.replace("l", "1")
+    if "o" in word:
+        word = word.replace("o","*")
+    return word
+
+
 def generate_weaker_password():
     word1=fetch_word()
     word2=fetch_word()
+    word1 = replaceLetters(word1)
+    word2 = replaceLetters(word2)
     password = word1 + word2
     return password
 
